d4a3ec949a722136679d9c678153c12c66cbef85
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2022-05-18 22:31:06 +0900

    gg

---
+++ paintjs/app.js
@@ -0,0 +1,40 @@
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if (!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }
+    else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+
+}
+
+if (canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
